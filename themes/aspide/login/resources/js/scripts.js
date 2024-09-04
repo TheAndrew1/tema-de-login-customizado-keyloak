44fc9f6c8a9f696606286ef0fcd3332b4459c69b
Andre <andre.velasco@itaipuparquetec.org.br>
2024-09-04 09:38:09 -0300

    ajuste no botão de ver senha e cor do alerta de erro

--- themes/aspide/login/resources/js/scripts.js
+++ themes/aspide/login/resources/js/scripts.js
@@ -1,41 +1,26 @@
 window.onload = function() {
+    eye = {
+      full: "M572.5 241.4C518.3 135.6 410.9 64 288 64S57.7 135.6 3.5 241.4a32.4 32.4 0 0 0 0 29.2C57.7 376.4 165.1 448 288 448s230.3-71.6 284.5-177.4a32.4 32.4 0 0 0 0-29.2zM288 400a144 144 0 1 1 144-144 143.9 143.9 0 0 1 -144 144zm0-240a95.3 95.3 0 0 0 -25.3 3.8 47.9 47.9 0 0 1 -66.9 66.9A95.8 95.8 0 1 0 288 160z",
+      slash: "M320 400c-75.9 0-137.3-58.7-142.9-133.1L72.2 185.8c-13.8 17.3-26.5 35.6-36.7 55.6a32.4 32.4 0 0 0 0 29.2C89.7 376.4 197.1 448 320 448c26.9 0 52.9-4 77.9-10.5L346 397.4a144.1 144.1 0 0 1 -26 2.6zm313.8 58.1l-110.6-85.4a331.3 331.3 0 0 0 81.3-102.1 32.4 32.4 0 0 0 0-29.2C550.3 135.6 442.9 64 320 64a308.2 308.2 0 0 0 -147.3 37.7L45.5 3.4A16 16 0 0 0 23 6.2L3.4 31.5A16 16 0 0 0 6.2 53.9l588.4 454.7a16 16 0 0 0 22.5-2.8l19.6-25.3a16 16 0 0 0 -2.8-22.5zm-183.7-142l-39.3-30.4A94.8 94.8 0 0 0 416 256a94.8 94.8 0 0 0 -121.3-92.2A47.7 47.7 0 0 1 304 192a46.6 46.6 0 0 1 -1.5 10l-73.6-56.9A142.3 142.3 0 0 1 320 112a143.9 143.9 0 0 1 144 144c0 21.6-5.3 41.8-13.9 60.1z"
+    }
+
     var fToggle = function($password, e) {
       e.preventDefault();
       var path = document.getElementById("caminhoSvg");
-      const type = $password.getAttribute('type') === 'password' ? 'text' : 'password';
-      $password.setAttribute('type', type);
-      if(path.getAttribute("d") == eye[0]) {
-        path.setAttribute("d", eye[1]);
-      } else {
-        path.setAttribute("d", eye[0]);
-      }
+      const type = $password.getAttribute("type") === "password" ? "text" : "password";
+      $password.setAttribute("type", type);
+      const pathData = path.getAttribute("d") == eye.full ? eye.slash: eye.full;
+      path.setAttribute("d", pathData);
     };
-
-    eye = [
-        "M572.5 241.4C518.3 135.6 410.9 64 288 64S57.7 135.6 3.5 241.4a32.4 32.4 0 0 0 0 29.2C57.7 376.4 165.1 448 288 448s230.3-71.6 284.5-177.4a32.4 32.4 0 0 0 0-29.2zM288 400a144 144 0 1 1 144-144 143.9 143.9 0 0 1 -144 144zm0-240a95.3 95.3 0 0 0 -25.3 3.8 47.9 47.9 0 0 1 -66.9 66.9A95.8 95.8 0 1 0 288 160z",
-        "M320 400c-75.9 0-137.3-58.7-142.9-133.1L72.2 185.8c-13.8 17.3-26.5 35.6-36.7 55.6a32.4 32.4 0 0 0 0 29.2C89.7 376.4 197.1 448 320 448c26.9 0 52.9-4 77.9-10.5L346 397.4a144.1 144.1 0 0 1 -26 2.6zm313.8 58.1l-110.6-85.4a331.3 331.3 0 0 0 81.3-102.1 32.4 32.4 0 0 0 0-29.2C550.3 135.6 442.9 64 320 64a308.2 308.2 0 0 0 -147.3 37.7L45.5 3.4A16 16 0 0 0 23 6.2L3.4 31.5A16 16 0 0 0 6.2 53.9l588.4 454.7a16 16 0 0 0 22.5-2.8l19.6-25.3a16 16 0 0 0 -2.8-22.5zm-183.7-142l-39.3-30.4A94.8 94.8 0 0 0 416 256a94.8 94.8 0 0 0 -121.3-92.2A47.7 47.7 0 0 1 304 192a46.6 46.6 0 0 1 -1.5 10l-73.6-56.9A142.3 142.3 0 0 1 320 112a143.9 143.9 0 0 1 144 144c0 21.6-5.3 41.8-13.9 60.1z"
-    ]
   
-    var createReveal = function(passwordId, toggleId) {
+    var createReveal = function(passwordId) {
       var password = document.getElementById(passwordId);
-      if ((password) && (password.style) && (password.style.display !== 'none')){
-        var icon = document.createElement("span");
-        icon.id = "password-toggle";
-        icon.classList.add("pf-c-input-group__text", "no-border");
-        var svg = document.createElement("svg");
-        svg.id = "password-icon";
-        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-        svg.setAttribute("viewBox", "0 0 576 512");
-        var path = document.createElement("path");
-        path.id = "caminhoSvg";
-        path.setAttribute("fill", "#1351b4");
-        path.setAttribute("d", eye[0]);
-        svg.appendChild(path);
-        icon.appendChild(svg);
-        icon.addEventListener('click', fToggle.bind(icon, password)); 
+      if ((password) && (password.style) && (password.style.display !== "none")){
+        var icon = document.getElementById("password-toggle");
+        icon.addEventListener("click", fToggle.bind(icon, password)); 
         password.parentNode.insertBefore(icon, password.nextSibling);
       }
     };
   
-    createReveal('password', 'togglePassword');
+    createReveal("password");
 }
